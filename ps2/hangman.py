b62fe0f33e96f0e1526e0f5d4ab348bd3b6e1768
Riley Rockwell <rileyrockwell@gmail.com>
2023-02-20 09:17:42 -0800

    initial commit

--- ps2/hangman.py
+++ ps2/hangman.py
@@ -354,6 +354,7 @@ if __name__ == "__main__":
     # To test part 3 re-comment out the above lines and 
     # uncomment the following two lines. 
 
+    # print("testing")
 
     # print(match_with_gaps('t_ _ ting', 'testing'))
     # print(match_with_gaps('testing', 'testing'))
