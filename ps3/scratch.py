3c90b245c06a6924b18f0371497ddb6cb15174ba
Riley Rockwell <64986809+rileyrockwell@users.noreply.github.com>
2023-03-01 03:10:11 +0000

    update

--- ps3/scratch.py
+++ ps3/scratch.py
@@ -1,17 +1,16 @@
-    sum_of_letter_values = 0
-    for i in word.lower():
-        # obtain the value of the key:value pair from the SCRABBLE_LETTERS_DICTIONARY
-        sum_of_letter_values += SCRABBLE_LETTER_VALUES[i]
+import time
 
-    abstract_sum = 7 * len(word) - 3 * (n - len(word))
-    if abstract_sum > 1:
-        abstract_sum = abstract_sum
+def powers(a,b):
+    return a**b
+
+for i in range(2, 11):
+    print("2**" + str(i) + ":", powers(2,i))
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
     else:
-        abstract_sum = 1
-
-    return sum_of_letter_values * abstract_sum
-
-
-# print(get_word_score('aaa', 3))
-
-print(1)
+        return n * factorial(n - 1)
+    
+for i in range(2, 11):
+    print(str(i) + "!: ", factorial(i))
