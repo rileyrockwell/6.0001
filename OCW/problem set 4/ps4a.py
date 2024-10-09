633bbccdc3cdacd633de172daf187de5f102e49e
Riley Rockwell <rileyrockwell@gmail.com>
2024-10-09 12:39:39 +0000

    updates

--- OCW/problem set 4/ps4a.py
+++ OCW/problem set 4/ps4a.py
@@ -22,15 +22,23 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
-
-    pass #delete this line and replace with your code here
+    if len(sequence) == 1:
+        return [sequence]
+    
+    permutations = []
+    for i, letter in enumerate(sequence):
+        for perm in get_permutations(sequence[:i] + sequence[i+1:]):
+            permutations.append(letter + perm)
+    
+    return permutations
+   
 
 if __name__ == '__main__':
-#    #EXAMPLE
-#    example_input = 'abc'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-#    print('Actual Output:', get_permutations(example_input))
+    #EXAMPLE
+    example_input = 'abc'
+    print('Input:', example_input)
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations(example_input))
     
 #    # Put three example test cases here (for your sanity, limit your inputs
 #    to be three characters or fewer as you will have n! permutations for a 
