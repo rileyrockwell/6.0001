923f81f8d358500ba8bb4681576843111dded622
Riley Rockwell <rileyrockwell@gmail.com>
2024-10-18 13:49:39 -0700

    updates

--- edX/UNIT_6/finger_exercises/scratch.py
+++ edX/UNIT_6/finger_exercises/scratch.py
@@ -14,3 +14,30 @@ def program1(x):
         total += x
 
     return total
+
+
+def program2(x):
+    total = 0
+    for i in range(1000):
+        total = i
+
+    while x > 0:
+        x = x//2
+        total += x
+
+    return total
+
+
+def program3(L):
+    totalSum = 0
+    highestFound = None
+    for x in L:
+        totalSum += x
+
+    for x in L:
+        if highestFound == None:
+            highestFound = x
+        elif x > highestFound:
+            highestFound = x
+
+    return (totalSum, highestFound)
