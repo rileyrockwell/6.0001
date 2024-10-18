a7cc236ec96c84e2f47b86b5e2c683893aabfdae
Riley Rockwell <rileyrockwell@gmail.com>
2024-10-18 10:35:03 -0700

    updates

---
+++ edX/UNIT_6/finger_exercises/scratch.py
@@ -0,0 +1,16 @@
+def linearSearch(L, x):
+	for e in L:
+		if e == x:
+			return True
+	return False
+
+def program1(x):
+    total = 0
+    for i in range(1000):
+        total += i
+
+    while x > 0:
+        x -= 1
+        total += x
+
+    return total
