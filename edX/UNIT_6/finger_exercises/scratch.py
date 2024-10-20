bb67d14576a64dc0c4c15aa72134fffc85c96cea
Riley Rockwell <rileyrockwell@gmail.com>
2024-10-19 22:49:26 -0700

    updates

---
+++ edX/UNIT_6/finger_exercises/scratch.py
@@ -0,0 +1,58 @@
+def selSort(L):
+    for i in range(len(L) - 1):
+        minIndx = i
+        minVal = L[i]
+        j = i+1
+        while j < len(L):
+            if minVal > L[j]:
+                minIndx = j
+                minVal = L[j]
+            j += 1
+        if minIndx != i:
+            temp = L[i]
+            L[i] = L[minIndx]
+            L[minIndx] = temp
+
+    return L
+
+
+def newSort(L):
+    for i in range(len(L) - 1):
+        j=i+1
+        while j < len(L):
+            if L[i] > L[j]:
+                temp = L[i]
+                L[i] = L[j]
+                L[j] = temp
+            j += 1
+    return L
+
+
+def mySort(L):
+    """ L, list with unique elements """
+    clear = False
+    while not clear:
+        clear = True
+        for j in range(1, len(L)):
+            if L[j-1] > L[j]:
+                clear = False
+                temp = L[j]
+                L[j] = L[j-1]
+                L[j-1] = temp
+
+
+def newSort(L):
+    """ L, list with unique elements """
+    for i in range(len(L) - 1):
+        j=i+1
+        while j < len(L):
+            if L[i] > L[j]:
+                temp = L[i]
+                L[i] = L[j]
+                L[j] = temp
+            j += 1
+
+
+L = [i for i in range(10) if i % 2 == 0]
+print(selSort(L))
+print(newSort(L))
